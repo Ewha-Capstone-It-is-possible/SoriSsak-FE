777d25de858067ae44b446916d97b13af7135684
bhyerain <seunghye0307@naver.com>
2025-12-24 22:06:53 +0900

    chore: 스타일 속성 변경 및 라우터 추가

--- app/(auth)/index.tsx
+++ app/(auth)/index.tsx
@@ -1,11 +1,11 @@
-import { StyleSheet, Text, View, Image, Pressable } from "react-native";
-import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import { SafeAreaView } from "react-native-safe-area-context";
 import InputBox from "@/components/InputBox";
-import { FONT } from "@/constants/font";
 import MainButton from "@/components/MainButton";
+import { FONT } from "@/constants/font";
+import { LinearGradient } from "expo-linear-gradient";
 import { useRouter } from "expo-router";
+import React from "react";
+import { Image, Pressable, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Login = () => {
   const router = useRouter();
@@ -17,7 +17,7 @@ const Login = () => {
 
   return (
     <LinearGradient
-      colors={["#FFF8DF", "#FFF8DF", "#DBFFB5"]}
+      colors={["#FCF8EB", "#FCF8EB", "#DBFFB5"]}
       locations={[0, 0.5, 1]}
       start={{ x: 0, y: 0 }}
       end={{ x: 0, y: 1 }}
@@ -90,13 +90,14 @@ const Login = () => {
             <MainButton size="medium" selected>
               카카오로 시작하기
             </MainButton>
-            <Pressable>
+            <Pressable onPress={() => router.push("/(auth)/SignUp")}>
               <Text
                 style={{
                   color: "#949494",
                   fontFamily: FONT.pretendard.regular,
                   fontSize: 12,
                   textAlign: "center",
+                  textDecorationLine: "underline",
                 }}
               >
                 회원가입
